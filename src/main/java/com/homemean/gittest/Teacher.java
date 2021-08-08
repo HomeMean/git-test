964627556963233d75fde59a6dee273c680cec99
”程家洺“ <15071472812@163.com>
2021-08-08 08:18:05 +0800

    second commit

--- src/main/java/com/homemean/gittest/Teacher.java
+++ src/main/java/com/homemean/gittest/Teacher.java
@@ -3,5 +3,8 @@ package com.homemean.gittest;
 public class Teacher {
     public static void main(String[] args) {
         System.out.println("this a teacher class");
+        System.out.println("this a teacher class");
+        System.out.println("this a teacher class");
+        System.out.println("this a teacher class");
     }
 }
